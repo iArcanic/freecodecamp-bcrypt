5c4c7cd8ae087cc9f2dfe8ade98d756a714d7133
Preetham Ananthkumar <preetham.mervin@gmail.com>
2024-07-17 10:26:07 +0000

    Tweaked synchronous hash and result variables

--- server.js
+++ server.js
@@ -11,20 +11,20 @@ const someOtherPlaintextPassword = "pass123";
 
 // START_ASYNC
 bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
-    console.log('Async hash: ' + hash);
+    console.log("Async hash: " + hash);
 
     bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
-        console.log('Async result: ' +res);
-    })
+        console.log("Async result: " + res);
+    });
 });
 // END_ASYNC
 
 // START_SYNC
-let hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
-console.log('Sync hash: ' + hash);
+var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+console.log("Sync hash: " + hash);
 
-let res = bcrypt.compareSync(myPlaintextPassword, hash);
-console.log('Sync result: ' + res);
+var res = bcrypt.compareSync(myPlaintextPassword, hash);
+console.log("Sync result: " + res);
 // END_SYNC
 
 const PORT = process.env.PORT || 3000;
