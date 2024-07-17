de4dd22a725b8b671b60fac1a0829d0eec23eb5e
Preetham Ananthkumar <preetham.mervin@gmail.com>
2024-07-17 10:22:50 +0000

    Hashing and comparing passwords synchronously

--- server.js
+++ server.js
@@ -10,19 +10,21 @@ const myPlaintextPassword = "sUperpassw0rd!";
 const someOtherPlaintextPassword = "pass123";
 
 // START_ASYNC
-
 bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
-    console.log(hash);
+    console.log('Async hash: ' + hash);
 
     bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
-        console.log(res);
+        console.log('Async result: ' +res);
     })
 });
-
 // END_ASYNC
 
 // START_SYNC
+let hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+console.log('Sync hash: ' + hash);
 
+let res = bcrypt.compareSync(myPlaintextPassword, hash);
+console.log('Sync result: ' + res);
 // END_SYNC
 
 const PORT = process.env.PORT || 3000;
